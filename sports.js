295ed6ba54d138ff976a1f414861047bd040680c
HenorAbdyli <142497426+HenorAbdyli@users.noreply.github.com>
2024-01-28 17:41:55 +0100

    Add files via upload

---
+++ sports.js
@@ -0,0 +1,13 @@
+// fetch("https://v3.football.api-sports.io/fixtures/rounds?season=2019&league=61", {
+//     "method": "GET",
+//     "headers": {
+//         "x-rapidapi-host": "v3.football.api-sports.io",
+//         "x-rapidapi-key": "XxXxXxXxXxXxXxXxXxXxXxXx"
+//     }
+// })
+//     .then(response => {
+//         console.log(response);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
